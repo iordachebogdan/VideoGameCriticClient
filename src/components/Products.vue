8bb69ac886e433fd83c56f9c68bc6085945e7f71
Bogdan Iordache <iordache_bogdan1998@yahoo.com>
2018-12-30 00:31:25 +0200

    Add filter by category for games

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
+    <div v-if="isGame">
+      <div
+        v-for="category in categories"
+        :key="category.id"
+        class="custom-control custom-checkbox custom-control-inline"
+      >
+        <input type="checkbox" class="custom-control-input" :id="category.id" :value="category.id" v-model="activeCategories">
+        <label class="custom-control-label" :for="category.id">{{category.name}}</label>
+      </div>
+    </div>
     <div v-for="product in products" :key="product.id">
       {{product.name}}
     </div>
@@ -11,7 +21,10 @@ export default {
   name: 'products',
   data () {
     return {
-      products: []
+      products: [],
+      categories: [],
+      activeCategories: [],
+      isGame: false
     }
   },
   methods: {
@@ -33,7 +46,6 @@ export default {
           url += 'category_ids[]=' + this.$route.query.categories[i]
         }
       }
-      console.log(url)
       return url
     },
     fetchProducts () {
@@ -41,10 +53,41 @@ export default {
         .then(function (response) {
           this.products = response.body.data
         })
+    },
+    fetchCategories () {
+      this.$http.get('http://localhost:3000/api/categories')
+        .then(function (response) {
+          this.categories = response.body.data
+        })
     }
   },
   created: function () {
     this.fetchProducts()
+    this.fetchCategories()
+    if (this.$route.params.type === 'games') {
+      this.isGame = true
+    }
+    if (this.$route.query.categories) {
+      for (let i = 0; i < this.$route.query.categories.length; ++i) {
+        this.activeCategories.push(parseInt(this.$route.query.categories[i]))
+      }
+    }
+  },
+  watch: {
+    activeCategories: function (newCategories) {
+      let queryConfig = {}
+      if (this.$route.query.platform) {
+        queryConfig.platform = this.$route.query.platform
+      }
+      if (newCategories.length !== 0) {
+        queryConfig.categories = newCategories
+      }
+      this.$router.push(
+        queryConfig === {}
+          ? { path: `/${this.$route.params.type}` }
+          : { path: `/${this.$route.params.type}`, query: queryConfig }
+      )
+    }
   }
 }
 </script>
