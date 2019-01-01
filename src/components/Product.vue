54d4ae292fa6288e697852b2693e76ed236a046f
Bogdan Iordache <iordache_bogdan1998@yahoo.com>
2019-01-01 23:17:26 +0200

    CRUD for comments and delete product

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -3,19 +3,20 @@
     <mdb-row>
       <mdb-card class="product-card" color="elegant-color">
         <mdb-card-body class="operation-container">
-          <mdb-btn color="danger" class="operation">Delete</mdb-btn>
+          <mdb-btn color="danger" class="operation" @click.native="modal = true">Delete</mdb-btn>
           <mdb-btn color="info" class="operation">Edit</mdb-btn>
         </mdb-card-body>
         <mdb-card-image v-bind:src="image" class="product-image" waves></mdb-card-image>
         <mdb-card-body class="text-center" cascade>
           <mdb-card-title class="product-name"><strong>{{product.name}}</strong></mdb-card-title>
           <div class="product-meta">
-            <i class="fa fa-star-o" aria-hidden="true">{{product.avgRating}}</i>
+            <i class="fa fa-star-o" aria-hidden="true">{{avgRating}}</i>
           </div>
           <div class="product-meta">
             <div v-if="product.category">{{product.category.name}}</div>
             <div>{{product.platform.name}}</div>
           </div>
+          <br><br>
           <h4 class="white-text"><strong>Description</strong></h4>
           <mdb-card-text class="white-text">
             <p class="product-description">{{product.description}}</p>
@@ -25,30 +26,86 @@
     </mdb-row>
 
     <mdb-row>
-      <mdb-card class="comment-form" color="elegant-color">
-        <mdb-card-title>
-          Add a comment
-          <mdb-btn class="operation">Add</mdb-btn>
+      <mdb-card class="comment-form" color="elegant-color" id="add-edit-comment-form">
+        <mdb-card-title class="comment-card-title">
+          {{isEditing === -1 ? 'Add a' : 'Update'}} comment
+          <mdb-btn
+            v-if="isEditing === -1"
+            class="operation"
+            :color="comment.user !== '' && comment.text !== '' ? 'success' : ''"
+            @click.native.prevent="addComment"
+          >
+            Add
+          </mdb-btn>
+          <mdb-btn
+            v-else
+            class="operation"
+            :color="comment.user !== '' && comment.text !== '' ? 'success' : ''"
+            @click.native.prevent="editCommentSave"
+          >
+            Save
+          </mdb-btn>
         </mdb-card-title>
         <mdb-card-body>
           <label for="userNameForm" class="grey-text">Your name</label>
-          <input type="text" id="userNameForm" class="form-control">
+          <input v-model="comment.user" type="text" id="userNameForm" class="form-control black-text">
           <br>
           <label for="commentForm" class="grey-text">Your comment</label>
-          <textarea type="text" id="commentForm" class="form-control" rows="3"></textarea>
+          <textarea v-model="comment.text" type="text" id="commentForm" class="form-control black-text" rows="3"></textarea>
           <br>
           <label class="grey-text">Your rating</label>
-          <star-rating v-model="rating" :star-size="40"></star-rating>
+          <star-rating v-model="comment.rating" :star-size="40"></star-rating>
         </mdb-card-body>
       </mdb-card>
     </mdb-row>
+
+    <mdb-row>
+      <mdb-card class="comment-form" color="elegant-color" v-for="comment in product.comments" :key="comment.id">
+        <mdb-card-title class="comment-card-title">
+          {{comment.user}}
+          <mdb-btn class="operation"
+            color="danger"
+            @click.native.prevent="deleteComment(comment.id)"
+          >
+            Delete
+          </mdb-btn>
+          <mdb-btn class="operation"
+            color="info"
+            @click.native.prevent="editCommentInit(comment.id)"
+          >
+            Edit
+          </mdb-btn>
+        </mdb-card-title>
+        <mdb-card-body style="padding-top: 0;">
+          <star-rating :rating="comment.rating" :read-only="true" :star-size="20"></star-rating>
+          <div class="comment-text">
+          {{comment.text}}
+          </div>
+        </mdb-card-body>
+      </mdb-card>
+    </mdb-row>
+
+    <mdb-modal v-if="modal" @close="modal = false">
+      <mdb-modal-header>
+        <mdb-modal-title>Warning!</mdb-modal-title>
+      </mdb-modal-header>
+      <mdb-modal-body>Are you sure you want to delete this
+        {{$route.params.type === 'games' ? 'game' : 'console'}}?
+      </mdb-modal-body>
+      <mdb-modal-footer>
+        <mdb-btn color="info" @click.native="modal = false">Cancel</mdb-btn>
+        <mdb-btn color="danger" @click.native="deleteProduct">Delete</mdb-btn>
+      </mdb-modal-footer>
+    </mdb-modal>
   </mdb-container>
 </template>
 
 <script>
 import { mdbCard, mdbCardImage, mdbCardBody, mdbCardTitle, mdbCardText,
-  mdbContainer, mdbRow, mdbView, mdbMask, mdbBtn } from 'mdbvue'
+  mdbContainer, mdbRow, mdbView, mdbMask, mdbBtn, mdbModal, mdbModalHeader,
+  mdbModalTitle, mdbModalBody, mdbModalFooter } from 'mdbvue'
 import StarRating from 'vue-star-rating'
+import VueScrollTo from 'vue-scrollto'
 
 export default {
   name: 'product',
@@ -63,15 +120,28 @@ export default {
     mdbView,
     mdbMask,
     mdbBtn,
-    StarRating
+    StarRating,
+    VueScrollTo,
+    mdbModal,
+    mdbModalHeader,
+    mdbModalTitle,
+    mdbModalBody,
+    mdbModalFooter
   },
   data () {
     return {
       product: {
         category: {},
-        platform: {}
+        platform: {},
+        comments: []
       },
-      rating: 1
+      comment: {
+        user: '',
+        text: '',
+        rating: 1
+      },
+      isEditing: -1,
+      modal: false
     }
   },
   computed: {
@@ -79,6 +149,14 @@ export default {
       return this.product.image_url === ''
         ? '/static/default_product.png'
         : 'http://localhost:3000' + this.product.image_url
+    },
+    avgRating: function () {
+      let sum = 0
+      for (let i = 0; i < this.product.comments.length; ++i) {
+        sum += this.product.comments[i].rating
+      }
+      let mean = sum * 1.0 / this.product.comments.length
+      return (sum === 0 ? 0 : Math.round(mean * 10) / 10)
     }
   },
   methods: {
@@ -86,16 +164,73 @@ export default {
       this.$http.get(`http://localhost:3000/api/${this.$route.params.type}/${this.$route.params.id}`)
         .then(function (response) {
           this.product = response.body.data
-          this.product.avgRating = this.getAvgRating(this.product)
         })
     },
-    getAvgRating (product) {
-      let sum = 0
-      for (let i = 0; i < product.comments.length; ++i) {
-        sum += product.comments[i].rating
+    addComment () {
+      if (this.comment.user === '' || this.comment.text === '') {
+        return
       }
-      let mean = sum * 1.0 / product.comments.length
-      return (sum === 0 ? 0 : Math.round(mean * 10) / 10)
+      let newComment = {
+        user: this.comment.user,
+        text: this.comment.text,
+        rating: this.comment.rating,
+        product_id: this.$route.params.id
+      }
+      this.product.comments.push(newComment)
+      this.$http.post(`http://localhost:3000/api/comments`, newComment)
+        .then(function (response) {
+          newComment.id = response.body.data.id
+        })
+      this.comment.user = ''
+      this.comment.text = ''
+      this.comment.rating = 1
+    },
+    deleteComment (id) {
+      this.product.comments = this.product.comments.filter((product) => {
+        return product.id !== id
+      })
+      this.$http.delete(`http://localhost:3000/api/comments/${id}`)
+    },
+    editCommentInit (id) {
+      this.isEditing = id
+      let comm = this.product.comments.find(comment => {
+        return comment.id === id
+      })
+      this.comment = {
+        user: comm.user,
+        text: comm.text,
+        rating: comm.rating
+      }
+      VueScrollTo.scrollTo('#add-edit-comment-form')
+    },
+    editCommentSave () {
+      if (this.comment.user === '' || this.comment.text === '') {
+        return
+      }
+      for (let i = 0; i < this.product.comments.length; ++i) {
+        if (this.product.comments[i].id === this.isEditing) {
+          this.product.comments[i].user = this.comment.user
+          this.product.comments[i].text = this.comment.text
+          this.product.comments[i].rating = this.comment.rating
+
+          this.$http.put(`http://localhost:3000/api/comments/${this.isEditing}`, {
+            user: this.comment.user,
+            text: this.comment.text,
+            rating: this.comment.rating
+          })
+          break
+        }
+      }
+      this.isEditing = -1
+      this.comment.user = ''
+      this.comment.text = ''
+      this.comment.rating = 1
+    },
+    deleteProduct () {
+      this.$http.delete(`http://localhost:3000/api/${this.$route.params.type}/${this.$route.params.id}`)
+        .then(function (response) {
+          this.$router.push({ path: `/${this.$route.params.type}` })
+        })
     }
   },
   created: function () {
@@ -157,6 +292,16 @@ export default {
   margin-right: auto;
   width: 80%;
   padding: 0.5em;
+  color: black;
+}
+
+.comment-text {
+  white-space: pre-line;
+}
+
+.comment-card-title {
+  margin-bottom: 0;
+  padding-left: 0.5em;
 }
 </style>
 
